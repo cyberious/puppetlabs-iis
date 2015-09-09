49e91a5e2fa09863722353e9da96863cfd030cbc
Travis Fields <travis@puppetlabs.com>
2015-09-09 16:34:40 -0700

    (maint) Add facter test and remove unused code

--- spec/acceptance/iis_install_spec.rb
+++ spec/acceptance/iis_install_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper_acceptance'
 
 RSpec.describe 'iis::install' do
-  def create_apply_manifest(params)
-    pp = "class {'wsus_client':"
-    params.each { |k, v|
-      v = "'#{v}'" if v.is_a? String
-      pp << "\n  #{k.to_s} => #{v},"
-    }
-    pp << "}"
-    apply_manifest_on(default, pp)
-  end
 
   describe 'install IIS' do
     it {
@@ -26,5 +17,13 @@ RSpec.describe 'iis::install' do
     end
   end
 
-
+  describe 'fact' do
+    it 'should report back a version' do
+      distmoduledir = on(default, "echo #{default['distmoduledir']}").raw_output.chomp
+      facter_opts = {:environment => {'FACTERLIB' => "#{distmoduledir}/iis/lib/facter"}}
+      fact_on(default, 'iis_version', facter_opts) do |result|
+        expect(result.raw_output).to match(/^\d\.\d/)
+      end
+    end
+  end
 end
